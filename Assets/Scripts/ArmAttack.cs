58d03c1299d2aafe66b0c9b676fd04d4b1c2d022
Jenny Jiao Hsia <jj1317@nyu.edu>
2015-11-15 00:53:11 -0500

    test out arm flailing in testlevelARMS scene

---
+++ Assets/Scripts/ArmAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmAttack : MonoBehaviour {
+
+	public GameObject leftShoulder;
+
+	public GameObject rightShoulder;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+#if UNITY_STANDALONE_OSX
+		if (Input.GetButton ("LeftBumper_OSX")) {
+			leftShoulder.transform.Rotate(10f, 0f, 0f);
+		}
+#endif
+
+#if UNITY_STANDALONE_OSX
+		if (Input.GetButton ("RightBumper_OSX")) {
+			rightShoulder.transform.Rotate(-10f, 0f, 0f);
+		}
+#endif
+	
+	}
+}
