c2fa8af7acbce3a30dd9835056d7bd3a9dbcb5cf
Kong Huang <kong.huang@nyu.edu>
2015-11-17 14:31:49 -0500

    added very very very basic attacking logic

--- Assets/Scripts/FacetedWater.cs
+++ Assets/Scripts/FacetedWater.cs
@@ -23,7 +23,7 @@ public class FacetedWater : MonoBehaviour {
 	void Update() {
 		mesh = GetComponent<MeshFilter>().mesh;
 		Vector3[] vertices = mesh.vertices;
-		Vector3[] normals = mesh.normals;
+		//Vector3[] normals = mesh.normals;
 		int i = 0;
 		while (i < vertices.Length) {
 			vertices[i].y = 1 * Mathf.PingPong(Time.time*speed, randomTimes[i]);
