d84c379e10cbadd0dcef42172d8cdc72e48174a8
Kong Huang <kh1534@nyu.edu>
2015-11-23 20:11:56 -0500

    Multiple local player movement, collision doesn't work though...

--- Assets/Scripts/CollisionLogic.cs
+++ Assets/Scripts/CollisionLogic.cs
@@ -1,25 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CollisionLogic : MonoBehaviour {
-
 	int hitcount;
-
 	void Start(){
 		hitcount = 3;
 	}
 
 	void OnTriggerEnter(Collider collision){
-
 		Debug.Log ("COLLISION!");
+		Debug.Log (collision.gameObject.name);
 		if (collision.gameObject.tag == "Arm") {
 			Debug.Log ("ARM!");
 			hitcount--;
 
 			if (hitcount <= 0){
-				Destroy (GetComponent<HingeJoint>());
-				GetComponent<Rigidbody>().AddForce((new Vector3(0f, 1f, -1f)) * 1000f);
+				// knock top player off
+				//GetComponent<Rigidbody>().AddForce((new Vector3(0f, 1f, -1f)) * 1000f);
 			}
 		}
 	}
+	
 }
