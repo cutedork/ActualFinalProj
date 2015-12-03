f377facb8077b1d9cf0f477bdfff992336cd3bed
Kong Huang <kong.huang@nyu.edu>
2015-12-03 14:50:34 -0500

    Finished camera zoom script might need some very minor tweaks.

--- Assets/Scripts/CameraZoom.cs
+++ Assets/Scripts/CameraZoom.cs
@@ -3,77 +3,40 @@ using System.Collections;
 
 public class CameraZoom : MonoBehaviour {
 
-	// attach to main camera
-	public Transform player1Pos; 
-	public Transform player2Pos;
+	public Transform player1;
+	public Transform player2;
 
-	[SerializeField]
-	Transform[] targets;
-
-	[SerializeField] 
-	float boundingBoxPadding = 2f;
+	Vector3 midpoint;
+	Vector3 offset;
+	float minimumOrthoSize;
+	float distance;
+	float cameraSpeed;
 	
-	[SerializeField]
-	float minimumOrthographicSize = 8f;
-	
-	[SerializeField]
-	float zoomSpeed = 20f;
-	
-	Camera movingCamera;
-
-
 	// Use this for initialization
 	void Start () {
-		movingCamera = Camera.main; 
-		targets = new Transform[] {player1Pos, player2Pos};
+		offset = new Vector3 (142f, 132f, -170f);
+		minimumOrthoSize = 20f;
 	}
 
 	void LateUpdate()
 	{
-		Rect boundingBox = CalculateTargetsBoundingBox();
-		transform.position = CalculateCameraPosition(boundingBox);
-		movingCamera.orthographicSize = CalculateOrthographicSize(boundingBox);
+		transform.position = CalculateCameraPosition();
+		//Debug.Log (Vector3.Distance (player1.transform.position, player2.transform.position));
+		GetComponent<Camera>().orthographicSize = CalculateOrthographicSize();
 	}
 
-	// calculate the orthographic "box" that has the objects you want to see
-	Rect CalculateTargetsBoundingBox()
-	{
-		float minX = Mathf.Infinity;
-		float maxX = Mathf.NegativeInfinity;
-		float minY = Mathf.Infinity;
-		float maxY = Mathf.NegativeInfinity;
-		
-		foreach (Transform target in targets) {
-			Vector3 position = target.position;
-			
-			minX = Mathf.Min(minX, position.x);
-			minY = Mathf.Min(minY, position.y);
-			maxX = Mathf.Max(maxX, position.x);
-			maxY = Mathf.Max(maxY, position.y);
-		}
-		
-		return Rect.MinMaxRect(minX - boundingBoxPadding, maxY + boundingBoxPadding, maxX + boundingBoxPadding, minY - boundingBoxPadding);
+
+	Vector3 CalculateCameraPosition(){
+		midpoint = new Vector3 ((player1.transform.position.x + player2.transform.position.x) / 2,
+		                        (player1.transform.position.y + player2.transform.position.y) / 2,
+		                        (player1.transform.position.z + player2.transform.position.z) / 2);
+		Debug.DrawLine(player1.transform.position, midpoint, Color.red);
+		return (midpoint + offset);
 	}
 
-	Vector3 CalculateCameraPosition(Rect boundingBox)
-	{
-		Vector2 boundingBoxCenter = boundingBox.center;
-		
-		return new Vector3(boundingBoxCenter.x, boundingBoxCenter.y, movingCamera.transform.position.z);
-	}
-
-	float CalculateOrthographicSize(Rect boundingBox)
-	{
-		float orthographicSize = movingCamera.orthographicSize;
-		Vector3 topRight = new Vector3(boundingBox.x + boundingBox.width, boundingBox.y, 0f);
-		Vector3 topRightAsViewport = movingCamera.WorldToViewportPoint(topRight);
-		
-		if (topRightAsViewport.x >= topRightAsViewport.y)
-			orthographicSize = Mathf.Abs(boundingBox.width) / movingCamera.aspect / 2f;
-		else
-			orthographicSize = Mathf.Abs(boundingBox.height) / 2f;
-		
-		return Mathf.Clamp(Mathf.Lerp(movingCamera.orthographicSize, orthographicSize, Time.deltaTime * zoomSpeed), minimumOrthographicSize, Mathf.Infinity);
+	float CalculateOrthographicSize(){
+		distance = Vector3.Distance(player1.transform.position, player2.transform.position);
+		return (minimumOrthoSize + (distance/7f));
 	}
 
 }
